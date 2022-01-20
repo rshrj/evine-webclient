5d0ea64e9f9320e902acef72aa45ea7792361e48
Bimalesh Seth <sethbimalesh@users.noreply.github.com>
2022-01-20 15:58:17 +0530

    refactored forgotPassword page, timer in resend button

--- src/pages/ForgotPassword/ForgotPassword.js
+++ src/pages/ForgotPassword/ForgotPassword.js
@@ -52,10 +52,9 @@ const ForgotPassword = () => {
 
   const errors = useSelector((store) => store.errors.formErrors);
 
-  const { requestSent, isValidToken, resetDone } = useSelector(
-    (store) => store.auth.forgotPassword
-  );
-
+  //states : DEFAULT, REQUEST_SENT, VALID_TOKEN, INVALID_TOKEN, REST_DONE
+  const [state, setState] = useState('DEFAULT');
+ 
   const loading = useSelector((store) => store.auth.loading === 'loading');
 
   const loggedIn = useSelector((store) => store.auth.loading === 'loggedIn');
@@ -70,7 +69,7 @@ const ForgotPassword = () => {
 
   useEffect(() => {
     if (verificationToken) {
-      dispatch(verifyResetToken({ token: verificationToken }));
+      dispatch(verifyResetToken({setState, token: verificationToken }));
     }
     if (loggedIn) {
       navigate(from, { replace: true });
@@ -87,19 +86,19 @@ const ForgotPassword = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    dispatch(forgotPassword({ email: values.email }));
+    dispatch(forgotPassword({ setTimer, setState, email: values.email }));
   };
 
     const resendEmail = (event) => {
       event.preventDefault();
-      setTimer(60);
-      dispatch(forgotPassword({ email: values.email }));
+      dispatch(forgotPassword({ setTimer, setState, email: values.email }));
     };
 
   const handleResetSubmit = (event) => {
     event.preventDefault();
     dispatch(
       resetPassword({
+        setState,
         token: verificationToken,
         password: values.password,
         password2: values.password2,
@@ -133,260 +132,286 @@ const ForgotPassword = () => {
         />
       </Box>
     );
-  } else if (!verificationToken) {
-    let data;
-    if (requestSent) {
-      data = (
-        <>
+  } else if(state === 'DEFAULT' || state === 'REQUEST_SENT'){
+          content = (
+            <>
+              <Link
+                underline='hover'
+                component={RouterLink}
+                variant='body2'
+                to='/'
+                sx={{
+                  display: 'inline-flex',
+                  alignItems: 'center',
+                  marginBottom: 2,
+                  color: 'primary.main',
+                }}>
+                <FaArrowLeft style={{ color: 'inherit' }} />
+                <Typography sx={{ marginLeft: 1, color: 'inherit' }}>
+                  Back to Home
+                </Typography>
+              </Link>
+              <Typography
+                variant='h3'
+                sx={{
+                  marginBottom: 4,
+                  fontWeight: 'bold',
+                }}>
+                Forgot Password
+              </Typography>
+
+              <FormGroup>
+                {state === 'DEFAULT' ? (
+                  <>
+                    <JInputField
+                      topLabel='Your Registered Email'
+                      placeholder='Enter your email'
+                      value={values.email}
+                      handleChange={handleChange('email')}
+                      errors={errors['email']}
+                      disabled={loading}
+                    />
+
+                    <FormControl
+                      sx={{
+                        color: 'text.primary',
+                        mb: 2,
+                        mt: 3,
+                      }}>
+                      <Button
+                        disabled={loading}
+                        variant='contained'
+                        sx={{
+                          boxShadow: 'none',
+                          '&:hover': {
+                            boxShadow: 'none',
+                          },
+                        }}
+                        onClick={handleSubmit}>
+                        {loading ? <Loader /> : 'Submit'}
+                      </Button>
+                    </FormControl>
+                  </>
+                ) : (
+                  <>
+                    <Typography
+                      variant='body1'
+                      sx={{
+                        mb: 4,
+                        mt: 2,
+                        fontWeight: 'bold',
+                        color: '#28a745',
+                        textAlign: 'center',
+                      }}>
+                      Please check your email.
+                    </Typography>
+                    <FormControl
+                      sx={{
+                        textAlign: 'center',
+                        mb: 2,
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                      }}>
+                      <Button
+                        disabled={timer !== 0}
+                        variant='contained'
+                        sx={{
+                          boxShadow: 'none',
+                          '&:hover': {
+                            boxShadow: 'none',
+                          },
+                        }}
+                        onClick={resendEmail}
+                        color='primary'>
+                        {timer !== 0 ? `Resend in ${timer}` : 'Resend email'}
+                      </Button>
+                    </FormControl>
+                  </>
+                )}
+                <FormControl
+                  sx={{
+                    textAlign: 'center',
+                  }}>
+                  <Link
+                    component={RouterLink}
+                    to='/login'
+                    underline='hover'
+                    color='primary'>
+                    Log in here
+                  </Link>
+                </FormControl>
+
+                <FormControl
+                  sx={{
+                    textAlign: 'center',
+                  }}>
+                  <Link
+                    component={RouterLink}
+                    to='/signup'
+                    underline='hover'
+                    color='primary'>
+                    New here? Sign up
+                  </Link>
+                </FormControl>
+              </FormGroup>
+            </>
+          );
+  } else if(state === 'INVALID_TOKEN'){
+      content = (
+        <Box
+          component='div'
+          sx={{
+            display: 'flex',
+            width: '100%',
+            height: '100%',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
           <Typography
             variant='body1'
             sx={{
               mb: 4,
               mt: 2,
               fontWeight: 'bold',
-              color: '#28a745',
+              color: '#dc3545',
             }}>
-            Please check your email.
+            Invalid Link.
           </Typography>
-          <FormControl
+          <Typography
+            variant='body1'
             sx={{
-              textAlign: 'center',
-              mb: 2,
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'center',
+              mb: 4,
+              mt: 2,
+              fontWeight: 'bold',
+              color: '#dc3545',
             }}>
-            <Button
-              disabled={timer !== 0}
-              variant='contained'
-              sx={{
-                width: '150px',
-                boxShadow: 'none',
-                '&:hover': {
-                  boxShadow: 'none',
-                },
-              }}
-              onClick={resendEmail}
-              color='primary'>
-              {timer !== 0 ? timer : 'Resend email'}
-            </Button>
-          </FormControl>
-        </>
+            Please close the window.
+          </Typography>
+        </Box>
       );
-    } else {
-      data = (
-        <>
-          <JInputField
-            topLabel='Your Registered Email'
-            placeholder='Enter your email'
-            value={values.email}
-            handleChange={handleChange('email')}
-            errors={errors['email']}
-            disabled={loading}
-          />
-
-          <FormControl
-            sx={{
-              color: 'text.primary',
-              mb: 2,
-              mt: 3,
-            }}>
-            <Button
-              disabled={loading}
-              variant='contained'
+  } else if(state === 'VALID_TOKEN'){
+        content = (
+          <>
+            <Typography
+              variant='h3'
               sx={{
-                boxShadow: 'none',
-                '&:hover': {
-                  boxShadow: 'none',
-                },
-              }}
-              onClick={handleSubmit}>
-              {loading ? <Loader /> : 'Submit'}
-            </Button>
-          </FormControl>
-        </>
-      );
-    }
+                marginBottom: 4,
+                fontWeight: 'bold',
+              }}>
+              Reset Password
+            </Typography>
+            <FormGroup>
+              <JPasswordField
+                topLabel='New Password'
+                placeholder='Enter your password'
+                value={values.password}
+                handleChange={handleChange('password')}
+                errors={errors.password}
+                handleClickShowPassword={handleClickShowPassword(
+                  'showPassword'
+                )}
+                showPassword={values.showPassword}
+                disabled={loading}
+              />
 
-    content = (
-      <>
-        <Link
-          underline='hover'
-          component={RouterLink}
-          variant='body2'
-          to='/'
-          sx={{
-            display: 'inline-flex',
-            alignItems: 'center',
-            marginBottom: 2,
-          }}>
-          <FaArrowLeft />
-          <Typography sx={{ marginLeft: 1 }}>Back to Home</Typography>
-        </Link>
-        <Typography
-          variant='h5'
-          sx={{
-            marginBottom: 4,
-            fontWeight: 'bold',
-          }}>
-          Forgot Password
-        </Typography>
+              <JPasswordField
+                topLabel='Confirm New Password'
+                placeholder='Confirm your password'
+                value={values.password2}
+                handleChange={handleChange('password2')}
+                errors={errors.password2}
+                handleClickShowPassword={handleClickShowPassword(
+                  'showPassword2'
+                )}
+                showPassword={values.showPassword2}
+                disabled={loading}
+                defaultHelperText=''
+              />
 
-        <FormGroup>
-          {data}
-          <FormControl
-            sx={{
-              textAlign: 'center',
-            }}>
-            <Link
-              component={RouterLink}
-              to='/login'
-              underline='hover'
-              color='primary'>
-              Log in here
-            </Link>
-          </FormControl>
+              <FormControl
+                sx={{
+                  color: 'text.primary',
+                  mb: 2,
+                  mt: 3,
+                }}>
+                <Button
+                  disabled={loading}
+                  variant='contained'
+                  sx={{
+                    boxShadow: 'none',
+                    '&:hover': {
+                      boxShadow: 'none',
+                    },
+                  }}
+                  onClick={handleResetSubmit}>
+                  {loading ? <Loader /> : 'Submit'}
+                </Button>
+              </FormControl>
+            </FormGroup>
+          </>
+        );
+  } else if(state === 'REST_DONE'){
+     content = (
+       <>
+         <Typography
+           variant='body1'
+           sx={{
+             mb: 4,
+             mt: 2,
+             fontWeight: 'bold',
+             color: '#28a745',
+           }}>
+           Your password has been changed successfully.
+         </Typography>
 
-          <FormControl
-            sx={{
-              textAlign: 'center',
-            }}>
-            <Link
-              component={RouterLink}
-              to='/signup'
-              underline='hover'
-              color='primary'>
-              New here? Sign up
-            </Link>
-          </FormControl>
-        </FormGroup>
-      </>
-    );
-  } else if (!isValidToken) {
-    content = (
-      <Box
-        component='div'
-        sx={{
-          display: 'flex',
-          width: '100%',
-          height: '100%',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Typography
-          variant='body1'
-          sx={{
-            mb: 4,
-            mt: 2,
-            fontWeight: 'bold',
-            color: '#dc3545',
-          }}>
-          Invalid Link.
-        </Typography>
-        <Typography
-          variant='body1'
-          sx={{
-            mb: 4,
-            mt: 2,
-            fontWeight: 'bold',
-            color: '#dc3545',
-          }}>
-          Please close the window.
-        </Typography>
-      </Box>
-    );
-  } else if (!resetDone) {
-    content = (
-      <>
-        <Typography
-          variant='h5'
-          sx={{
-            marginBottom: 4,
-            fontWeight: 'bold',
-          }}>
-          Reset Password
-        </Typography>
-        <FormGroup>
-          <JPasswordField
-            topLabel='New Password'
-            placeholder='Enter your password'
-            value={values.password}
-            handleChange={handleChange('password')}
-            errors={errors.password}
-            handleClickShowPassword={handleClickShowPassword('showPassword')}
-            showPassword={values.showPassword}
-            disabled={loading}
-          />
-
-          <JPasswordField
-            topLabel='Confirm New Password'
-            placeholder='Confirm your password'
-            value={values.password2}
-            handleChange={handleChange('password2')}
-            errors={errors.password2}
-            handleClickShowPassword={handleClickShowPassword('showPassword2')}
-            showPassword={values.showPassword2}
-            disabled={loading}
-            defaultHelperText=''
-          />
-
-          <FormControl
-            sx={{
-              color: 'text.primary',
-              mb: 2,
-              mt: 3,
-            }}>
-            <Button
-              disabled={loading}
-              variant='contained'
-              sx={{
-                boxShadow: 'none',
-                '&:hover': {
-                  boxShadow: 'none',
-                },
-              }}
-              onClick={handleResetSubmit}>
-              {loading ? <Loader /> : 'Submit'}
-            </Button>
-          </FormControl>
-        </FormGroup>
-      </>
-    );
+         <FormControl
+           sx={{
+             textAlign: 'center',
+           }}>
+           <Link
+             component={RouterLink}
+             to='/login'
+             underline='hover'
+             color='primary'>
+             Click here to Login
+           </Link>
+         </FormControl>
+       </>
+     );
   } else {
-    content = (
-      <>
-        <Typography
-          variant='body1'
-          sx={{
-            mb: 4,
-            mt: 2,
-            fontWeight: 'bold',
-            color: '#28a745',
-          }}>
-          Your password has been changed successfully.
-        </Typography>
-
-        <FormControl
-          sx={{
-            textAlign: 'center',
-          }}>
-          <Link
-            component={RouterLink}
-            to='/login'
-            underline='hover'
-            color='primary'>
-            Click here to Login
-          </Link>
-        </FormControl>
-      </>
-    );
+     content = (
+       <Box
+         component='div'
+         sx={{
+           display: 'flex',
+           width: '100%',
+           height: '100%',
+           flexDirection: 'column',
+           justifyContent: 'center',
+           alignItems: 'center',
+         }}>
+         <Typography
+           variant='body1'
+           sx={{
+             mb: 4,
+             mt: 2,
+             fontWeight: 'bold',
+             color: '#dc3545',
+           }}>
+           Something went wrong. Close this window.
+         </Typography>
+       </Box>
+     );
   }
+  
   return (
     <>
       <Grid container sx={{ backgroundColor: 'white' }}>
-        <Grid item container xs={12} sm={8} lg={6} justifyContent='center'>
-          <Grid item>
+        <Grid item container xs={12} sm={8} lg={6}>
+          <Grid item xs={2} sm={2} lg={3} xl={4} />
+          <Grid item xs={8} sm={8} lg={6} xl={4}>
             <Box
               sx={{
                 display: 'flex',
@@ -397,6 +422,7 @@ const ForgotPassword = () => {
               {content}
             </Box>
           </Grid>
+          <Grid item xs={2} sm={2} lg={3} xl={4} />
         </Grid>
         <Background />
       </Grid>
